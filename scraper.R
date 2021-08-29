48225f92e1f34c2dbd10e007675a9d16753adc61
Nathan Kim <nathanckim18@gmail.com>
2021-08-29 15:41:56 -0400

    unescaped html

--- scraper.R
+++ scraper.R
@@ -1,15 +1,21 @@
 library(tidyverse)
 library(magrittr)
+library(xml2)
 library(jsonlite)
 temp <- tempfile()
 download.file('https://www.marxists.org/subject/quotes/quote-abs.js', 
               temp)
 
+unescape_html2 <- function(str){
+  html <- paste0("<x>", paste0(str, collapse = "#_|"), "</x>")
+  parsed <- xml2::xml_text(xml2::read_html(html))
+  strsplit(parsed, "#_|", fixed = TRUE)[[1]]
+}
+
 readChar(temp, file.info(temp)$size) %>% 
   str_match_all("(?<=<p>)(.*?)(?=</p>)") %>% 
   extract2(1) %>% 
   map_dfr(function(quote){
-    message()
     body <- str_extract(quote, ".*<br />") %>% 
       str_remove_all("<(.*?)>")
     link <- str_extract(quote, '(?<=href=\")(.*?)(?=\">)')
@@ -19,4 +25,5 @@ readChar(temp, file.info(temp)$size) %>%
            link = link,
            attribution = attribution)
   }) %>% 
+  mutate(across(.fns = unescape_html2)) %>% 
   write_json("quotes.json")
