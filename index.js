2fb35136f67fd64a4f12cc445671f9c4ce213bc5
Nathan Kim <nathanckim18@gmail.com>
2022-01-12 22:35:25 -0500

    cleanup

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+#! /usr/bin/env node
 import express from 'express'
 import cors from 'cors'
 import fs from 'fs'
+import {config} from 'dotenv'
+if (process.env.NODE_ENV !== 'production') {
+  config()
+}
 
 const quotes = JSON.parse(fs.readFileSync('quotes.json'))
 // using readFileSync because it's literally the only thing we're reading/importing/processing
@@ -17,18 +22,18 @@ const getDailyQuote = () => {
 const app = express()
 
 app.use(cors())
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   const quote = getRandomQuote()
   res.send(quote)
 })
 
-app.get('/daily', (req, res) => {
+app.get('/daily', (_, res) => {
   const quote = getDailyQuote()
   res.send(quote)
 })
 
-app.get('/all-quotes', (req, res) => {
+app.get('/all-quotes', (_, res) => {
   res.send(quotes)
 })
 
-app.listen(process.env.PORT || 80, () => {})
+app.listen(process.env.PORT || 80, () => console.log("listening on port", process.env.PORT))
